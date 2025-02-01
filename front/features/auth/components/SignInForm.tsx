8addc459fc67c89e89fb99d6f838d120d48ceb61
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-02 01:47:07 +0900

    [update] ログイン画面の作成

--- front/features/auth/components/SignInForm.tsx
+++ front/features/auth/components/SignInForm.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { auth } from "@/libs/firebase/client";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import Image from "next/image";
+import { Link, TextField } from "@mui/material";
 
 export const SignInForm = () => {
   const [email, setEmail] = useState("");
@@ -34,20 +36,111 @@ export const SignInForm = () => {
 
   return (
     <>
-      <div>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button onClick={handleSignIn}>Sign In</button>
+      <div className="flex">
+        <div className="flex-1 relative h-full">
+          <Image
+            src="/login_page.png"
+            alt="Login page image"
+            className="object-cover w-full h-full"
+            width={864}
+            height={800}
+          />
+        </div>
+        <div className="flex-1 flex items-center justify-center">
+          <div className="max-w-md p-8">
+            <h1 className="text-[40px] font-bold leading-[40px] tracking-[0.21666669845581055px] text-center font-['October_Devanagari'] mb-2">
+              おかえりなさい！
+            </h1>
+            <p className="text-[14px] font-bold leading-[14px] tracking-[0.21666669845581055px] text-center font-['October_Devanagari'] mb-8">
+              今日も素敵な写真をいっぱい投稿しましょう
+            </p>
+            <h2 className="text-2xl font-bold mb-6 text-center">ログイン</h2>
+            <div className="flex flex-col gap-6 w-[260px] mx-auto">
+              <TextField
+                fullWidth
+                label="メールアドレス*"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                  sx: {
+                    color: "#69BEEF",
+                  },
+                }}
+                sx={{
+                  height: "64px",
+                  "& .MuiInputLabel-shrink": {
+                    transform: "translate(14px, -9px) scale(0.75)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderWidth: "2px",
+                      borderColor: "#69BEEF",
+                    },
+                  },
+                }}
+              />
+              <TextField
+                fullWidth
+                type="password"
+                label="パスワード"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                  sx: {
+                    color: "#69BEEF",
+                  },
+                }}
+                sx={{
+                  height: "64px",
+                  "& .MuiInputLabel-shrink": {
+                    transform: "translate(14px, -9px) scale(0.75)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderWidth: "2px",
+                      borderColor: "#69BEEF",
+                    },
+                  },
+                }}
+              />
+              <button
+                onClick={handleSignIn}
+                style={{
+                  height: "56px",
+                }}
+                className="bg-[#69BEEF] text-white rounded-md hover:bg-[#5CAADB]"
+              >
+                ログイン
+              </button>
+              <p className="font-['October_Devanagari'] text-[10px] font-[400] leading-[16px] text-center">
+                アカウントをお持ちではありませんか？{" "}
+                <Link href="/register" className="underline">
+                  アカウント作成
+                </Link>
+              </p>
+              <p className="text-center text-[#000000] mt-[30px]">
+                -------------------または-------------------
+              </p>
+              <button
+                style={{
+                  border: "1px solid #B2B2B2",
+                  height: "56px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                className="text-[#000000] rounded-md hover:bg-gray-200"
+              >
+                <Image src="/google-icon.png" alt="Google Icon" width={24} height={24} />
+                <p style={{ marginLeft: "5px" }}>Googleで続行</p>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
