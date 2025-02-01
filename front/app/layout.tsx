92099798db04d7d83d7134b9622f0664828cfa01
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-02 01:45:36 +0900

    [update] 基本背景色の修正と全体レイアウトの修正

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "@/features/auth/components/providers/SessionProvider";
+import { SessionProvider } from "@/features/auth/providers/SessionProvider";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
     <SessionProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          {children}
+          <Header />
+          <main className="pt-[50px]"> {children}</main>
+          <Footer />
         </body>
       </html>
     </SessionProvider>
