648632234d77a24d72383867e1c5c0f724de6571
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-07 05:40:36 +0900

    [update] 画面の分割とその他

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,28 @@
 import { Main } from "@/features/home/components/main";
-import { fetchNotifications, fetchLatestPosts } from "@/features/home/endpoint";
+import { fetchNotifications, fetchHomeFeed } from "@/features/home/endpoint";
+import { Post } from "@/features/home/types/post";
+import { Tag } from "@/features/home/types/tag";
 export default async function Home() {
   const notifications = await fetchNotifications();
-  const latestPosts = await fetchLatestPosts();
+  const homeData = await fetchHomeFeed<{
+    popularPostList: Post[];
+    latestPosts: Post[];
+    popularTagList: Tag[];
+    latestPostListWithX: Post[];
+  }>();
+  const { popularPostList, latestPosts, popularTagList, latestPostListWithX } = homeData;
+
   console.log(notifications);
   console.log(`latestPosts`);
   console.log(latestPosts);
-  return <Main notifications={notifications} latestPosts={latestPosts} />;
+  return (
+    <Main
+      notifications={notifications}
+      latestPosts={latestPosts}
+      popularTagList={popularTagList}
+      popularPostList={popularPostList}
+      latestPostListWithX={latestPostListWithX}
+    />
+  );
 }
 
---
+++ front/features/home/components/x-post.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import { Post } from "../types/post";
+
+export const XPost = ({
+  latestPostListWithX,
+  isLiked,
+  setIsLiked,
+}: {
+  latestPostListWithX: Post[];
+  isLiked: boolean;
+  setIsLiked: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <>
+      <div className="mx-6 bg-[#FFFFFF]">
+        <div className="max-w-full mx-auto pt-[40px] ">
+          <h2
+            className="text-[#151C4B] font-medium text-center"
+            style={{ fontWeight: "bold", fontSize: "40px", fontFamily: "Noto Sans JP" }}
+          >
+            Xからの投稿
+          </h2>
+          <div className="flex justify-center mt-6 mb-6 space-x-4">
+            {latestPostListWithX.map((post) => (
+              <div key={`${post.id}`} className="w-full relative">
+                <Image
+                  src="/home/x-post-sample.png"
+                  alt={`ピックアップ画像 ${post.id}`}
+                  width={402}
+                  height={384}
+                  className="w-full h-[384px] object-cover rounded-lg"
+                />
+                <div className="absolute bottom-4 right-4">
+                  <div
+                    className="flex items-center justify-center"
+                    style={{
+                      width: "64px",
+                      height: "64px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setIsLiked(!isLiked)}
+                  >
+                    {isLiked ? (
+                      <Image src="/heart.png" alt="heart" width={64} height={64} />
+                    ) : (
+                      <Image src="/heart-outline.png" alt="heart" width={64} height={64} />
+                    )}
+                  </div>
+                </div>
+                <div className="absolute" style={{ top: "8px", left: "8px" }}>
+                  <Image src="/home/x-icon.png" alt="Xからの投稿" width={80} height={80} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
