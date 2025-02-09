183c90673d15b0b713219dd57ce18f52014f0627
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-09 17:14:24 +0900

    [update] footerをscssに切り出した

---
+++ front/components/footer/footer.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import styles from "./styles.module.scss";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.serviceName}>VGRAM</div>
+        <div className={styles.links}>
+          <a href="/terms">利用規約</a>
+          <a href="/privacy">プライバシーポリシー</a>
+          <a href="/faq">Q&A</a>
+        </div>
+        <div className={styles.contact}>サービスに関するお問い合わせはsample@gmail.comまで</div>
+        <div className={styles.copyright}>© vgram.app All rights reserved.</div>
+      </div>
+    </footer>
+  );
+};
