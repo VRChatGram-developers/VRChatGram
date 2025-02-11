12a80d12e7bbfb3638a9c211b0fb6eeae2bf7c62
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-11 22:35:28 +0900

    [update] フォロー機能を呼ぶエンドポイントを追加

--- front/features/users/endpoint.ts
+++ front/features/users/endpoint.ts
@@ -1,6 +1,7 @@
 import { User } from "./types/user";
 
 const API_GET_URL = "http://localhost:3000";
+const API_POST_URL = "http://localhost:3001";
 
 export const fetchUserById = async (id: bigint): Promise<User> => {
     const response = await fetch(`${API_GET_URL}/api/users/${id}`);
@@ -10,3 +11,35 @@ export const fetchUserById = async (id: bigint): Promise<User> => {
     return response.json();
 };
 
+export const followUser = async (id: bigint) => {
+
+    try {
+        const response = await fetch(`${API_POST_URL}/api/users/${id}/followings`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to follow user");
+    }
+};
+
+export const unfollowUser = async (id: bigint) => {
+    try {
+        const response = await fetch(`${API_POST_URL}/api/users/${id}/followings`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to unfollow user");
+    }
+}
