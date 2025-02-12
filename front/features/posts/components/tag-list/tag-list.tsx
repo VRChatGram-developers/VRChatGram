a3e2616ff2e2e5b71373f56da20876cab46b4758
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-13 00:41:38 +0900

    [update] タグ一覧のコンポーネント追加

---
+++ front/features/posts/components/tag-list/tag-list.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styles from "./styles.module.scss";
+import { Tag } from "@/features/posts/types/tag";
+export const TagList = ({
+  popularTags,
+  selectedTag,
+  setSelectedTag,
+}: {
+  popularTags: Tag[];
+  selectedTag: string;
+  setSelectedTag: (tag: string) => void;
+}) => {
+  const handleTagClick = (tag: string) => {
+    setSelectedTag(tag);
+  };
+
+  return (
+    <div className={styles.tagList}>
+      {popularTags.map((tag) => (
+        <div
+          key={tag.id}
+          className={selectedTag === tag.name ? styles.selectedTagItem : styles.tagItem}
+        >
+          <button onClick={() => handleTagClick(tag.name)}>#{tag.name}</button>
+        </div>
+      ))}
+    </div>
+  );
+};
