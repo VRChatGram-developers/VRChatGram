fa9aa929aee4967265301e049e42f298adb4b617
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-14 02:34:23 +0900

    [update] エンドポイント呼び出す処理実装

--- front/features/posts/endpoint.ts
+++ front/features/posts/endpoint.ts
@@ -1,9 +1,8 @@
-import { Post } from "./types/post";
-import { Tag } from "./types/tag";
+import { Post, Tag, PostDetail } from "./types/index";
 
 export const fetchPosts = async (params: { page?: number; query?: string }): Promise<Post[][]> => {
   const response = await fetch(
-    `http://localhost:3000/api/posts/search?${
+    `http://localhost:3000/api/v1/posts/search?${
       params ? `page=${params.page}&query=${params.query}` : ""
     }`
   );
@@ -18,6 +17,15 @@ export const fetchPopularTags = async (): Promise<Tag[]> => {
   const response = await fetch(`http://localhost:3000/api/v1/tags/popular`);
   if (!response.ok) {
     throw new Error("Failed to fetch popular tags");
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const fetchPostById = async (id: string): Promise<PostDetail> => {
+  const response = await fetch(`http://localhost:3000/api/v1/posts/${id}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch post");
   }
   const data = await response.json();
   return data;
