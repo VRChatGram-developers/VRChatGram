534a17bb737a0ffaf45715f335e693da8c04b0ae
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-17 21:44:01 +0900

    [update] popular-post-listをcssに切り出す

--- nextjs/features/home/components/popular-post-list.tsx
+++ nextjs/features/home/components/popular-post-list.tsx
@@ -3,6 +3,7 @@
 import { PopularPost } from "../types/index";
 import _ from "lodash";
 import { PostCard } from "@/components/post-card";
+import styles from "../styles/popular-post-list.module.scss";
 
 export const PopularPostList = ({
   popularPostList,
@@ -19,33 +20,26 @@ export const PopularPostList = ({
 
   return (
     <>
-      <div style={{ padding: "3rem 1.5rem" }}>
-        <div className="w-full mx-auto">
-          <h2
-            className="text-[#151C4B] font-medium text-center"
-            style={{ fontWeight: "bold", fontSize: "40px", fontFamily: "Noto Sans JP" }}
-          >
-            ピックアップ
-          </h2>
-          {chunkPopularPostList(popularPostList).map((chunckedPost, index) => (
-            <div key={index} className="flex justify-center mt-6 mb-6 space-x-4">
-              {chunckedPost.map((post) => (
-                <PostCard
-                  key={`${index}-${post.id}`}
-                  postCardProps={{
-                    postName: post.title,
-                    postImageUrl: "/pickup-image.png",
-                    postImageCount: post.images.length,
-                    userName: post.user.name,
-                    userImageUrl: "/posts/sample-user-icon.png",
-                    isLiked: isLiked,
-                    setIsLiked: setIsLiked,
-                  }}
-                />
-              ))}
-            </div>
-          ))}
-        </div>
+      <div className={styles.popularPostListContainer}>
+        <p className={styles.popularPostListTitle}>ピックアップ</p>
+        {chunkPopularPostList(popularPostList).map((chunckedPost, index) => (
+          <div key={index} className={styles.popularPostList}>
+            {chunckedPost.map((post) => (
+              <PostCard
+                key={`${index}-${post.id}`}
+                postCardProps={{
+                  postName: post.title,
+                  postImageUrl: "/pickup-image.png",
+                  postImageCount: post.images.length,
+                  userName: post.user.name,
+                  userImageUrl: "/posts/sample-user-icon.png",
+                  isLiked: isLiked,
+                  setIsLiked: setIsLiked,
+                }}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </>
   );
