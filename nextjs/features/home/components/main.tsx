12281f90b78689785626a5d79d80f314be6b1916
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-17 21:31:05 +0900

    [update] mainをcssとコンポーネントに分ける

--- nextjs/features/home/components/main.tsx
+++ nextjs/features/home/components/main.tsx
@@ -10,6 +10,7 @@ import { LatestPost as LatestPostType, PopularPost as PopularPostType } from "..
 import { PopularPostList } from "./popular-post-list";
 import { LatestPost } from "./latest-post";
 import { XPost } from "./x-post";
+import styles from "../styles/main.module.scss";
 
 export const Main = ({
   popularPostList,
@@ -28,8 +29,8 @@ export const Main = ({
 
   return (
     <>
-      <div style={{ padding: "0px 1.5rem 1.5rem 3rem" }} className="bg-[#FFFFFF]">
-        <div className="max-w-full mx-auto">
+      <div className={styles.mainContainer}>
+        <div className={styles.topImage}>
           <div>
             <Image
               src="/top-image.png"
@@ -40,10 +41,10 @@ export const Main = ({
             />
           </div>
 
-          <div className="flex justify-center mt-6 mb-6 space-x-4">
-            <div className="flex-1 rounded-md">
-              <div className="flex space-x-4 mt-4 overflow-hidden">
-                <div className="w-1/2">
+          <div className={styles.mainImagesContainer}>
+            <div className={styles.mainImagesContainerFirst}>
+              <div className={styles.imageWrapperFirst}>
+                <div className={styles.imageFirst}>
                   <Image
                     src="/top-image.png"
                     alt="画像2"
@@ -52,7 +53,7 @@ export const Main = ({
                     className="w-full h-full object-cover rounded-lg opacity-70 hover:opacity-100"
                   />
                 </div>
-                <div className="w-1/2">
+                <div className={styles.imageSecond}>
                   <Image
                     src="/top-image.png"
                     alt="画像3"
@@ -64,9 +65,9 @@ export const Main = ({
               </div>
             </div>
 
-            <div className="flex-1 rounded-md">
-              <div className="flex space-x-4 mt-4 overflow-hidden">
-                <div className="w-1/2">
+            <div className={styles.mainImagesContainerSecond}>
+              <div className={styles.imageWrapperSecond}>
+                <div className={styles.imageThird}>
                   <Image
                     src="/top-image.png"
                     alt="画像2"
@@ -75,7 +76,7 @@ export const Main = ({
                     className="w-full h-full object-cover rounded-lg opacity-70 hover:opacity-100 "
                   />
                 </div>
-                <div className="w-1/2">
+                <div className={styles.imageFourth}>
                   <Image
                     src="/top-image.png"
                     alt="画像3"
@@ -87,23 +88,20 @@ export const Main = ({
               </div>
             </div>
           </div>
-
-          {/* 初めての方はこちら */}
-
-          <div className="flex justify-center mt-6 mb-6 space-x-4">
-            <div className="flex-1 rounded-md">
-              <div className="flex items-center">
-                <p className="font-semibold text-lg">初めての方はこちら</p>
+          <div className={styles.mainSecondContainer}>
+            <div className={styles.mainSecondContainerFirst}>
+              <div className={styles.mainSecondContainerFirstBox}>
+                <p className={styles.mainSecondContainerFirstText}>初めての方はこちら</p>
                 <Image
-                  src="/shoshinsha-mark.png" // 初心者マークの画像パスを指定
+                  src="/shoshinsha-mark.png"
                   alt="初心者マーク"
                   width={17}
                   height={17}
                   className="mr-2"
                 />
               </div>
-              <div className="flex space-x-4 mt-4 overflow-hidden">
-                <div className="w-1/2">
+              <div className={styles.mainSecondContainerSecondBox}>
+                <div className={styles.mainSecondContainerFirstImage}>
                   <Image
                     src="/top-image2.png"
                     alt="画像2"
@@ -112,7 +110,7 @@ export const Main = ({
                     className="w-full h-full object-cover rounded-lg"
                   />
                 </div>
-                <div className="w-1/2">
+                <div className={styles.mainSecondContainerSecondImage}>
                   <Image
                     src="/top-image3.png"
                     alt="画像3"
@@ -124,74 +122,45 @@ export const Main = ({
               </div>
             </div>
 
-            <div className="border-l border-[#D9D9D9] mx-4 self-stretch"></div>
+            <div className={styles.mainSecondContainerCentralLine}></div>
 
-            <div className="flex-1 rounded-md">
-              <p className="font-semibold text-lg">お知らせ</p>
-              <div className="">
-                {notifications.map((notification) => (
-                  <div key={notification.id} className="bg-white rounded-lg duration-200">
-                    <div className="pt-6">
-                      <div className="flex flex-col">
-                        <div className="flex items-center">
-                          <span
-                            style={{
-                              width: "118px",
-                              height: "56px",
-                              display: "flex",
-                              alignItems: "center",
-                              justifyContent: "center",
-                              borderRadius: "16px 16px 16px 16px",
-                              background:
-                                notification.notification_type === "release"
-                                  ? "#69BEEFCC"
-                                  : notification.notification_type === "important"
-                                  ? "#EBEF69CC"
-                                  : "rgba(234, 179, 8, 0.8)",
-                            }}
-                            className="text-[#000000] text-base font-medium"
-                          >
-                            {notification.notification_type}
-                          </span>
-                          <div className="ml-7 flex flex-col">
-                            <time
-                              className="text-sm text-gray-500"
-                              style={{
-                                fontSize: "12px",
-                                fontFamily: "Noto Sans JP",
-                                fontWeight: "bold",
-                                color: "#000000",
-                              }}
-                            >
-                              {notification.published_at}
-                            </time>
-                            {notification.content && (
-                              <p
-                                className="mt-1 text-sm text-gray-600"
-                                style={{
-                                  fontSize: "12px",
-                                  fontFamily: "Noto Sans JP",
-                                  whiteSpace: "pre-wrap",
-                                  fontWeight: "bold",
-                                  color: "#000000",
-                                }}
-                              >
-                                {notification.content}
-                              </p>
-                            )}
-                          </div>
-                        </div>
+            <div className={styles.mainSecondContainerThirdBox}>
+              <p className={styles.mainSecondContainerThirdBoxText}>お知らせ</p>
+              {notifications.map((notification) => (
+                <div key={notification.id} className={styles.notificationsList}>
+                  <div className={styles.notificationsListBox}>
+                    <div className={styles.notificationsListBoxText}>
+                      <span
+                        className={
+                          notification.notification_type === "release"
+                            ? styles.notificationsListBoxTextRelease
+                            : notification.notification_type === "important"
+                            ? styles.notificationsListBoxTextImportant
+                            : styles.notificationsListBoxTextCommon
+                        }
+                      >
+                        {notification.notification_type}
+                      </span>
+                      <div className={styles.notificationsContainer}>
+                        <time className={styles.notificationTime}>{notification.published_at}</time>
+                        {notification.content && (
+                          <p className={styles.notificationContent}>{notification.content}</p>
+                        )}
                       </div>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
 
-      <PopularPostList popularPostList={popularPostList} isLiked={isLiked} setIsLiked={setIsLiked} />
+      <PopularPostList
+        popularPostList={popularPostList}
+        isLiked={isLiked}
+        setIsLiked={setIsLiked}
+      />
       <PopularTag popularTagList={popularTagList} />
       <LatestPost latestPosts={latestPosts} isLiked={isLiked} setIsLiked={setIsLiked} />
       <XPost latestPostListWithX={latestPostListWithX} isLiked={isLiked} setIsLiked={setIsLiked} />
