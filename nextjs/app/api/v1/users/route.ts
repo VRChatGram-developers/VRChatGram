36ae5203e74cadb045fb40eeff8cbae635311387
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-19 00:33:17 +0900

    [update] user登録のAPIでuidを登録できるように修正

--- nextjs/app/api/v1/users/route.ts
+++ nextjs/app/api/v1/users/route.ts
@@ -14,10 +14,10 @@ export const connect = async () => {
 export async function POST(request: Request) {
   try {
     await connect();
-    const { name, password, email, introduce, gender, profile_url } = await request.json();
+    const { name, password, email, introduce, gender, profile_url, uid } = await request.json();
 
+    // TODO パスワードをHash化
     await prisma.users.create({
-
       data: {
         name: name,
         password: password,
@@ -29,39 +29,12 @@ export async function POST(request: Request) {
         birthday: new Date(),
         my_id: "my_id",
         show_sensitive: false,
+        uid: uid,
       },
     });
 
-    await connect();
     return NextResponse.json({ message: "ユーザー作成しました" });
   } catch (error) {
-    return new Error(`DB接続失敗しました: ${error}`);
-  }
-}
-
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-  try {
-    await connect();
-    const { id } = params;
-    if (!id) {
-      return NextResponse.json({ error: "idが指定されていません" }, { status: 400 });
-    }
-    const followUser = await prisma.follows.findFirst({
-      where: {
-        following_id: BigInt(Number(id)),
-        follower_id: 1,
-      },
-    });
-    if (!followUser) {
-      return NextResponse.json({ message: "フォローユーザー見つかりません" }, { status: 404 });
-    }
-    await prisma.follows.delete({
-      where: {
-        id: followUser.id,
-      },
-    });
-    return NextResponse.json({ message: "フォロー解除しました" });
-  } catch (error) {
-    return new Error(`DB接続失敗しました: ${error}`);
+    return NextResponse.json({ error: `ユーザー作成に失敗しました: ${error}` }, { status: 500 });
   }
 }
