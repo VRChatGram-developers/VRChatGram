a229e83ddca2783aa8b58c09693d3ff1d676fd60
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-19 00:18:51 +0900

    [update] 良いね機能のAPIを追加

--- nextjs/features/posts/endpoint.ts
+++ nextjs/features/posts/endpoint.ts
@@ -22,11 +22,28 @@ export const fetchPopularTags = async (): Promise<Tag[]> => {
   return data;
 };
 
-export const fetchPostById = async (id: string): Promise<PostDetail> => {
-  const response = await fetch(`http://localhost:3000/api/v1/posts/${id}`);
+export const fetchPostById = async (postId: string): Promise<PostDetail> => {
+  console.log(postId);
+  const response = await fetch(`http://localhost:3000/api/v1/posts/${postId}`);
   if (!response.ok) {
     throw new Error("Failed to fetch post");
   }
+  const data = await response.json();
+  return data;
+};
+
+export const likePost = async (postId: string) => {
+  const response = await fetch(`http://localhost:3000/api/v1/posts/${postId}/likes`, {
+    method: "POST",
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const unlikePost = async (postId: string) => {
+  const response = await fetch(`http://localhost:3000/api/v1/posts/${postId}/likes`, {
+    method: "DELETE",
+  });
   const data = await response.json();
   return data;
 };
