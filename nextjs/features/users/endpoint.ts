3967e3fc6823e98c55bc3c9914d9328d4fcbb4f9
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-19 00:35:50 +0900

    [update] エンドポイントを呼ぶ際の階層の修正とユーザー登録時にuidをapiに渡す

--- nextjs/features/users/endpoint.ts
+++ nextjs/features/users/endpoint.ts
@@ -5,7 +5,7 @@ import { auth } from "@/libs/firebase/client";
 const API_URL = "http://localhost:3000";
 
 export const fetchUserById = async (id: bigint): Promise<User> => {
-  const response = await fetch(`${API_URL}/api/users/${id}`);
+  const response = await fetch(`${API_URL}/api/v1/users/${id}`);
   if (!response.ok) {
     throw new Error("Failed to fetch users");
   }
@@ -14,7 +14,7 @@ export const fetchUserById = async (id: bigint): Promise<User> => {
 
 export const followUser = async (id: bigint) => {
   try {
-    const response = await fetch(`${API_URL}/api/users/${id}/followings`, {
+    const response = await fetch(`${API_URL}/api/v1/users/${id}/followings`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -30,7 +30,7 @@ export const followUser = async (id: bigint) => {
 
 export const unfollowUser = async (id: bigint) => {
   try {
-    const response = await fetch(`${API_URL}/api/users/${id}/followings`, {
+    const response = await fetch(`${API_URL}/api/v1/users/${id}/followings`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -45,16 +45,18 @@ export const unfollowUser = async (id: bigint) => {
 };
 
 export const createUser = async (user: requestCreateUser) => {
-  
   try {
-    console.log(user);
-    await createUserWithEmailAndPassword(auth, user.email, user.password);
+    const createdUser = await createUserWithEmailAndPassword(auth, user.email, user.password);
+
     const response = await fetch(`${API_URL}/api/v1/users`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify({
+        ...user,
+        uid: createdUser.user.uid,
+      }),
     });
     const data = await response.json();
     return data;
