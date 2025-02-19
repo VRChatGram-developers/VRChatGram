ef43cb14e469cdd2e2b641b0fec746e7fe0cdf46
Kazuya Uejima <>
2025-02-19 23:25:07 +0900

    【WIP】signup css

--- nextjs/features/auth/components/sign-up-form.tsx
+++ nextjs/features/auth/components/sign-up-form.tsx
@@ -56,120 +56,127 @@ export const SignUpForm = ({
   };
 
   return (
-    <>
-      <div className="flex">
-        <div className="flex-1 flex items-center justify-center">
-          <div className="max-w-md p-8">
-            <h1
-              style={{ fontSize: "40px" }}
-              className="font-bold leading-[40px] tracking-[0.21666669845581055px] text-center font-['October_Devanagari'] mb-2"
-            >
-              アカウントの作成
-            </h1>
+    <div className={styles.signupContainer}>
+      <div className={styles.signupContent}>
+        <div className={styles.signupFormContent}>
+          <h1
+            style={{ fontSize: "40px" }}
+            className="font-bold leading-[40px] tracking-[0.21666669845581055px] text-center font-['October_Devanagari'] mb-2"
+          >
+            アカウントの作成
+          </h1>
 
-            <div className="flex flex-col gap-6 w-[260px] mx-auto">
-              <TextField
-                fullWidth
-                label="メールアドレス*"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                variant="outlined"
-                style={{ marginTop: "48px" }}
-                InputLabelProps={{
-                  shrink: true,
-                  sx: {
-                    color: "#69BEEF",
+          <div className="flex flex-col gap-6 w-[260px] mx-auto">
+            <TextField
+              fullWidth
+              label="メールアドレス*"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              variant="outlined"
+              style={{ marginTop: "48px" }}
+              InputLabelProps={{
+                shrink: true,
+                sx: {
+                  color: "#69BEEF",
+                },
+              }}
+              sx={{
+                height: "64px",
+                "& .MuiInputLabel-shrink": {
+                  transform: "translate(14px, -9px) scale(0.75)",
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderWidth: "2px",
+                    borderColor: "#69BEEF",
                   },
-                }}
-                sx={{
-                  height: "64px",
-                  "& .MuiInputLabel-shrink": {
-                    transform: "translate(14px, -9px) scale(0.75)",
+                },
+              }}
+            />
+            {errorMail && (
+              <p className={styles.errorMailMessage}>{errorMail}</p>
+            )}
+            <TextField
+              fullWidth
+              type="password"
+              label="パスワード"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+                sx: {
+                  color: "#69BEEF",
+                },
+              }}
+              sx={{
+                height: "64px",
+                "& .MuiInputLabel-shrink": {
+                  transform: "translate(14px, -9px) scale(0.75)",
+                },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": {
+                    borderWidth: "2px",
+                    borderColor: "#69BEEF",
                   },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderWidth: "2px",
-                      borderColor: "#69BEEF",
-                    },
-                  },
-                }}
+                },
+              }}
+            />
+            {errorPassword && (
+              <p className={styles.errorPasswordMessage}>{errorPassword}</p>
+            )}
+            <button
+              onClick={handleSignUp}
+              style={{
+                height: "56px",
+                marginTop: "8px",
+              }}
+              className="bg-[#69BEEF] text-white rounded-md hover:bg-[#5CAADB]"
+            >
+              作成する
+            </button>
+            <p className="font-['October_Devanagari'] text-[10px] font-[400] leading-[16px] text-center">
+              既にアカウントをお持ちですか？{" "}
+              <Link href="/sign-in" className="underline">
+                ログイン
+              </Link>
+            </p>
+            <p
+              style={{ marginTop: "16px", fontSize: "10px" }}
+              className="text-center text-[#000000] mt-[30px]"
+            >
+              ---------------------------または---------------------------
+            </p>
+            <button
+              style={{
+                border: "1px solid #B2B2B2",
+                height: "56px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              className="text-[#000000] rounded-md hover:bg-gray-200"
+            >
+              <Image
+                src="/google-icon.png"
+                alt="Google Icon"
+                width={24}
+                height={24}
               />
-              {errorMail && <p className={styles.errorMailMessage}>{errorMail}</p>}
-              <TextField
-                fullWidth
-                type="password"
-                label="パスワード"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                variant="outlined"
-                InputLabelProps={{
-                  shrink: true,
-                  sx: {
-                    color: "#69BEEF",
-                  },
-                }}
-                sx={{
-                  height: "64px",
-                  "& .MuiInputLabel-shrink": {
-                    transform: "translate(14px, -9px) scale(0.75)",
-                  },
-                  "& .MuiOutlinedInput-root": {
-                    "& fieldset": {
-                      borderWidth: "2px",
-                      borderColor: "#69BEEF",
-                    },
-                  },
-                }}
-              />
-              {errorPassword && <p className={styles.errorPasswordMessage}>{errorPassword}</p>}
-              <button
-                onClick={handleSignUp}
-                style={{
-                  height: "56px",
-                  marginTop: "8px",
-                }}
-                className="bg-[#69BEEF] text-white rounded-md hover:bg-[#5CAADB]"
-              >
-                作成する
-              </button>
-              <p className="font-['October_Devanagari'] text-[10px] font-[400] leading-[16px] text-center">
-                既にアカウントをお持ちですか？{" "}
-                <Link href="/sign-in" className="underline">
-                  ログイン
-                </Link>
-              </p>
-              <p
-                style={{ marginTop: "16px", fontSize: "10px" }}
-                className="text-center text-[#000000] mt-[30px]"
-              >
-                ---------------------------または---------------------------
-              </p>
-              <button
-                style={{
-                  border: "1px solid #B2B2B2",
-                  height: "56px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-                className="text-[#000000] rounded-md hover:bg-gray-200"
-              >
-                <Image src="/google-icon.png" alt="Google Icon" width={24} height={24} />
-                <p style={{ marginLeft: "5px" }}>Googleで続行</p>
-              </button>
-            </div>
+              <p style={{ marginLeft: "5px" }}>Googleで続行</p>
+            </button>
           </div>
         </div>
-        <div className="flex-1 relative h-full">
-          <Image
-            src="/signup-icon.png"
-            alt="Login page image"
-            className="object-cover w-full h-full"
-            width={864}
-            height={800}
-          />
-        </div>
       </div>
-    </>
+      <div className={styles.registerImageContainer}>
+        <Image
+          src="/signup-icon.png"
+          alt="Login page image"
+          className="object-cover w-full h-full"
+          width={864}
+          height={800}
+        />
+      </div>
+    </div>
   );
 };
--- nextjs/features/users/components/account-info-input.tsx
+++ nextjs/features/users/components/account-info-input.tsx
@@ -6,7 +6,13 @@ import styles from "@/features/users/styles/users-register.module.scss";
 import { createUser } from "../endpoint";
 import { useRouter } from "next/navigation";
 
-export const AccountInfoInput = ({ email, password }: { email: string; password: string }) => {
+export const AccountInfoInput = ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
   const router = useRouter();
   const years = Array.from({ length: 100 }, (_, index) => 2025 - index); // 2025年から遡る
   const months = Array.from({ length: 12 }, (_, index) => index + 1); // 1月から12月
@@ -68,7 +74,12 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
     const isTermsValid = checkTerms();
 
     try {
-      if (!isNickNameValid || !isBirthdayValid || !isSexValid || !isTermsValid) {
+      if (
+        !isNickNameValid ||
+        !isBirthdayValid ||
+        !isSexValid ||
+        !isTermsValid
+      ) {
         return;
       }
 
@@ -90,35 +101,43 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
   };
 
   return (
-    <>
-      <div className="flex">
-        <div className="flex-1 flex items-center justify-center">
-          <div className={styles.registerContainer}>
-            <div className={styles.registerTitle}>
-              <p>アカウント作成</p>
-            </div>
-            <div className={styles.registerContent}>
-              <div className={styles.nickName}>
-                <div className={styles.nicknameLabel}>
-                  <p>ニックネーム</p>
-                </div>
-                <div className={styles.nicknameInput}>
-                  <input
-                    type="text"
-                    placeholder="ニックネーム"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <p>あなたの名前として表示されます</p>
-                {errorName && <p className={styles.errorNameMessage}>{errorName}</p>}
+    <div className={styles.signinContainer}>
+      <div className={styles.registerContainer}>
+        <div className={styles.registerContent}>
+          <div className={styles.registerTitleContent}>
+            <p className={styles.registerTitle}>アカウント作成</p>
+          </div>
+          <div className={styles.registerContentDetail}>
+            <div className={styles.nameContainer}>
+              <p className={styles.nameLabel}>ニックネーム</p>
+              <div className={styles.nameForm}>
+                <input
+                  type="text"
+                  placeholder="ニックネーム"
+                  value={name}
+                  className={styles.nameFormInput}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
-              <div>
+              <p className={styles.warningLabel}>
+                あなたの名前として表示されます
+              </p>
+              {errorName && (
+                <p className={styles.errorNameMessage}>{errorName}</p>
+              )}
+            </div>
+            <div className={styles.genderContainer}>
+              <p className={styles.genderLabel}>性別</p>
+              <div className={styles.genderSelectSection}>
                 {sexOptions.map((option) => (
-                  <label key={option.value} className="mr-4">
+                  <label
+                    key={option.value}
+                    className={styles.genderSelectLabel}
+                  >
                     <input
                       type="radio"
                       name="gender"
+                      className={styles.genderSelectInput}
                       value={option.value}
                       checked={selectedSex === option.value}
                       onChange={(e) => setSelectedSex(e.target.value)}
@@ -126,12 +145,20 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
                     {option.label}
                   </label>
                 ))}
-                {errorSex && <p className={styles.errorSexMessage}>{errorSex}</p>}
               </div>
-              <div>
-                <label htmlFor="birthday">誕生日</label>
-                <div>
-                  <select id="year" value={year} onChange={(e) => setYear(e.target.value)}>
+
+              {errorSex && <p className={styles.errorSexMessage}>{errorSex}</p>}
+            </div>
+            <div className={styles.birthdayContainer}>
+              <p className={styles.birthdayLabel}>誕生日</p>
+              <div className={styles.birthdayContent}>
+                <div className={styles.birthdaySelectContent}>
+                  <select
+                    id="year"
+                    className={styles.birthdaySelect}
+                    value={year}
+                    onChange={(e) => setYear(e.target.value)}
+                  >
                     <option value="">年</option>
                     {years.map((y) => (
                       <option key={y} value={y}>
@@ -139,8 +166,14 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
                       </option>
                     ))}
                   </select>
-
-                  <select id="month" value={month} onChange={(e) => setMonth(e.target.value)}>
+                </div>
+                <div className={styles.birthdaySelectContent}>
+                  <select
+                    id="month"
+                    className={styles.birthdaySelect}
+                    value={month}
+                    onChange={(e) => setMonth(e.target.value)}
+                  >
                     <option value="">月</option>
                     {months.map((m) => (
                       <option key={m} value={m}>
@@ -148,8 +181,14 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
                       </option>
                     ))}
                   </select>
-
-                  <select id="day" value={day} onChange={(e) => setDay(e.target.value)}>
+                </div>
+                <div className={styles.birthdaySelectContent}>
+                  <select
+                    id="day"
+                    className={styles.birthdaySelect}
+                    value={day}
+                    onChange={(e) => setDay(e.target.value)}
+                  >
                     <option value="">日</option>
                     {days.map((d) => (
                       <option key={d} value={d}>
@@ -157,47 +196,61 @@ export const AccountInfoInput = ({ email, password }: { email: string; password:
                       </option>
                     ))}
                   </select>
-                  {errorBirthday && <p className={styles.errorBirthdayMessage}>{errorBirthday}</p>}
+                  {errorBirthday && (
+                    <p className={styles.errorBirthdayMessage}>
+                      {errorBirthday}
+                    </p>
+                  )}
                 </div>
               </div>
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={termsChecked}
-                    onChange={(e) => setTermsChecked(e.target.checked)}
-                  />
-                  <span>
-                    <a href="/terms" target="_blank">
-                      利用規約
-                    </a>{" "}
-                    と
-                    <a href="/privacy" target="_blank">
-                      プライバシーポリシー
-                    </a>{" "}
-                    に同意する
-                  </span>
-                </label>
-                {errorTerms && <p className={styles.errorTermsMessage}>{errorTerms}</p>}
-              </div>
             </div>
-            <div>
-              <button onClick={handleCreateAccount} className={styles.userCreateButton}>
+            <div className={styles.confirmationContainer}>
+              <label className={styles.confirmationLabel}>
+                <input
+                  type="checkbox"
+                  className={styles.confirmationCheckbox}
+                  checked={termsChecked}
+                  onChange={(e) => setTermsChecked(e.target.checked)}
+                />
+                <span>
+                  <a href="/terms" target="_blank" className={styles.linkText}>
+                    利用規約
+                  </a>{" "}
+                  と
+                  <a
+                    href="/privacy"
+                    target="_blank"
+                    className={styles.linkText}
+                  >
+                    プライバシーポリシー
+                  </a>{" "}
+                  に同意する
+                </span>
+              </label>
+              {errorTerms && (
+                <p className={styles.errorTermsMessage}>{errorTerms}</p>
+              )}
+            </div>
+            <div className={styles.signupContent}>
+              <button
+                onClick={handleCreateAccount}
+                className={styles.userCreateButton}
+              >
                 アカウント作成
               </button>
             </div>
           </div>
         </div>
-        <div className="flex-1 relative h-full">
-          <Image
-            src="/signup-icon.png"
-            alt="Login page image"
-            className="object-cover w-full h-full"
-            width={864}
-            height={800}
-          />
-        </div>
       </div>
-    </>
+      <div className={styles.registerImageContainer}>
+        <Image
+          src="/signup-icon.png"
+          alt="Login page image"
+          className="object-cover w-full h-full"
+          width={864}
+          height={800}
+        />
+      </div>
+    </div>
   );
 };
