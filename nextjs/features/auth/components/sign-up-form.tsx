a065e71b5c838ff7a607b5d70be7aa5640cc01b8
Kazuya Uejima <>
2025-02-20 15:44:27 +0900

    【WIP】signup CSS変更

--- nextjs/features/auth/components/sign-up-form.tsx
+++ nextjs/features/auth/components/sign-up-form.tsx
@@ -66,105 +66,114 @@ export const SignUpForm = ({
             アカウントの作成
           </h1>
 
-          <div className="flex flex-col gap-6 w-[260px] mx-auto">
-            <TextField
-              fullWidth
-              label="メールアドレス*"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              variant="outlined"
-              style={{ marginTop: "48px" }}
-              InputLabelProps={{
-                shrink: true,
-                sx: {
-                  color: "#69BEEF",
-                },
-              }}
-              sx={{
-                height: "64px",
-                "& .MuiInputLabel-shrink": {
-                  transform: "translate(14px, -9px) scale(0.75)",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderWidth: "2px",
-                    borderColor: "#69BEEF",
+          <div className={styles.signupTextFormContainer}>
+            <div className={styles.textForm}>
+              <TextField
+                fullWidth
+                label="メールアドレス*"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                  sx: {
+                    color: "#69BEEF",
                   },
-                },
-              }}
-            />
-            {errorMail && (
-              <p className={styles.errorMailMessage}>{errorMail}</p>
-            )}
-            <TextField
-              fullWidth
-              type="password"
-              label="パスワード"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              variant="outlined"
-              InputLabelProps={{
-                shrink: true,
-                sx: {
-                  color: "#69BEEF",
-                },
-              }}
-              sx={{
-                height: "64px",
-                "& .MuiInputLabel-shrink": {
-                  transform: "translate(14px, -9px) scale(0.75)",
-                },
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderWidth: "2px",
-                    borderColor: "#69BEEF",
+                }}
+                sx={{
+                  height: "64px",
+                  "& .MuiInputLabel-shrink": {
+                    transform: "translate(14px, -9px) scale(0.75)",
                   },
-                },
-              }}
-            />
-            {errorPassword && (
-              <p className={styles.errorPasswordMessage}>{errorPassword}</p>
-            )}
-            <button
-              onClick={handleSignUp}
-              style={{
-                height: "56px",
-                marginTop: "8px",
-              }}
-              className="bg-[#69BEEF] text-white rounded-md hover:bg-[#5CAADB]"
-            >
-              作成する
-            </button>
-            <p className="font-['October_Devanagari'] text-[10px] font-[400] leading-[16px] text-center">
-              既にアカウントをお持ちですか？{" "}
-              <Link href="/sign-in" className="underline">
-                ログイン
-              </Link>
-            </p>
-            <p
-              style={{ marginTop: "16px", fontSize: "10px" }}
-              className="text-center text-[#000000] mt-[30px]"
-            >
-              ---------------------------または---------------------------
-            </p>
-            <button
-              style={{
-                border: "1px solid #B2B2B2",
-                height: "56px",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              className="text-[#000000] rounded-md hover:bg-gray-200"
-            >
-              <Image
-                src="/google-icon.png"
-                alt="Google Icon"
-                width={24}
-                height={24}
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderWidth: "2px",
+                      borderColor: "#69BEEF",
+                    },
+                  },
+                }}
               />
-              <p style={{ marginLeft: "5px" }}>Googleで続行</p>
-            </button>
+              {errorMail && (
+                <p className={styles.errorMailMessage}>{errorMail}</p>
+              )}
+            </div>
+
+            <div className={styles.textForm}>
+              <TextField
+                fullWidth
+                type="password"
+                label="パスワード"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                  sx: {
+                    color: "#69BEEF",
+                  },
+                }}
+                sx={{
+                  height: "64px",
+                  "& .MuiInputLabel-shrink": {
+                    transform: "translate(14px, -9px) scale(0.75)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderWidth: "2px",
+                      borderColor: "#69BEEF",
+                    },
+                  },
+                }}
+              />
+              <p className={styles.warningLabel}>
+                パスワードは半角英字、数字、記号を合わせた6文字以上で入力してください
+              </p>
+              {errorPassword && (
+                <p className={styles.errorPasswordMessage}>{errorPassword}</p>
+              )}
+            </div>
+            <div className={styles.createButtonContainer}>
+              <button onClick={handleSignUp} className={styles.createButton}>
+                作成する
+              </button>
+            </div>
+            <div className={styles.separateLoginContainer}>
+              <p className={styles.separateLoginText}>
+                既にアカウントをお持ちですか？{" "}
+                <Link href="/sign-in" className="underline">
+                  ログイン
+                </Link>
+              </p>
+            </div>
+            <div className={styles.separateLoginContainer}>
+              <p className={styles.separateLoginText}>
+                パスワードを忘れた方は{" "}
+                <Link href="/sign-in" className="underline">
+                  こちら
+                </Link>
+              </p>
+            </div>
+
+            <div>
+              <p
+                style={{ marginTop: "16px", fontSize: "10px" }}
+                className="text-center text-[#000000] mt-[30px]"
+              >
+                ---------------------------または---------------------------
+              </p>
+            </div>
+
+            <div className={styles.googleLoginContainer}>
+              <button className={styles.googleLoginButton}>
+                <Image
+                  src="/google-icon.png"
+                  alt="Google Icon"
+                  width={24}
+                  height={24}
+                />
+                <p className={styles.googleLoginText}>Googleで続行</p>
+              </button>
+            </div>
           </div>
         </div>
       </div>
