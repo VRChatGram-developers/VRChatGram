8afa032cf88c2153a9133baae2da385e862b7432
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-02-21 04:20:20 +0900

    [update] リファクタとログイン時、新規登録画面にはいけない

--- nextjs/app/signup/page.tsx
+++ nextjs/app/signup/page.tsx
@@ -1,5 +1,12 @@
 import { UserCreate } from "@/features/users/components/user-create";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect("/");
+  }
   return <UserCreate />;
 }
