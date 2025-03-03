67b0a08cf10d5d45f7cdc1f8e7bc08022d3e2956
Kazuya Uejima <>
2025-03-04 04:04:16 +0900

    【WIP】修正

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,30 +1,45 @@
 import { Main } from "@/features/home/components/main";
 import { fetchNotifications, fetchHomeFeed } from "@/features/home/endpoint";
 import { Tag } from "@/features/home/types/tag";
-import { PopularPost as PopularPostType, LatestPost as LatestPostType, XPost as XPostType } from "@/features/home/types/index";
+import {
+  PopularPost as PopularPostType,
+  LatestPost as LatestPostType,
+  XPost as XPostType,
+} from "@/features/home/types/index";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   const notifications = await fetchNotifications();
-  const serializedNotifications = notifications.notifications.map((notification) => {
-    const year = new Date(notification.published_at).getFullYear();
-    const month = String(new Date(notification.published_at).getMonth() + 1).padStart(2, "0"); // `01` 形式にする
-    const day = String(new Date(notification.published_at).getDate()).padStart(2, "0");
-    const formattedDate = `${year}.${month}.${day}`;
-    return {
-      ...notification,
-      published_at: formattedDate,
-    };
-  });
+  const serializedNotifications = notifications.notifications.map(
+    (notification) => {
+      const year = new Date(notification.published_at).getFullYear();
+      const month = String(
+        new Date(notification.published_at).getMonth() + 1
+      ).padStart(2, "0"); // `01` 形式にする
+      const day = String(
+        new Date(notification.published_at).getDate()
+      ).padStart(2, "0");
+      const formattedDate = `${year}.${month}.${day}`;
+      return {
+        ...notification,
+        published_at: formattedDate,
+      };
+    }
+  );
   const homeData = await fetchHomeFeed<{
     popularPostList: PopularPostType[];
     latestPostList: LatestPostType[];
     popularTagList: Tag[];
     latestPostListWithX: XPostType[];
   }>(session);
-  const { popularPostList, latestPostList, popularTagList, latestPostListWithX } = homeData;
+  const {
+    popularPostList,
+    latestPostList,
+    popularTagList,
+    latestPostListWithX,
+  } = homeData;
 
   return (
     <Main
@@ -36,4 +51,3 @@ export default async function Home() {
     />
   );
 }
-
