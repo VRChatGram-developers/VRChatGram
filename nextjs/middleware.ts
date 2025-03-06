0b93c693b4b47a19e91b1ef52aa63760687b117b
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-03-07 02:35:40 +0900

    [update] ミドルウェアの修正

--- nextjs/middleware.ts
+++ nextjs/middleware.ts
@@ -2,41 +2,40 @@ import { NextRequest, NextResponse } from "next/server";
 
 const allowedOrigins = ["https://vr-chat-gram.vercel.app", "http://localhost:3000"];
 
-const corsOptions = {
+const corsHeaders = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Credentials": "true", // 認証情報の送信を許可
 };
 
 export function middleware(request: NextRequest) {
-  // Check the origin from the request
   const origin = request.headers.get("origin") ?? "";
   const isAllowedOrigin = allowedOrigins.includes(origin);
-
-  // Handle preflighted requests
   const isPreflight = request.method === "OPTIONS";
 
   if (isPreflight) {
-    const preflightHeaders = {
+    const preflightHeaders = new Headers({
       ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
+      ...corsHeaders,
+    });
+
+    return new NextResponse(null, { headers: preflightHeaders, status: 204 });
   }
 
-  // Handle simple requests
   const response = NextResponse.next();
+  const headers = new Headers(response.headers);
 
   if (isAllowedOrigin) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
+    headers.set("Access-Control-Allow-Origin", origin);
   }
 
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
+  Object.entries(corsHeaders).forEach(([key, value]) => {
+    headers.set(key, value);
   });
 
-  return response;
+  return new NextResponse(response.body, { status: response.status, headers });
 }
 
 export const config = {
-  matcher: "/",
+  matcher: "/api/:path*", // API ルートに適用
 };
