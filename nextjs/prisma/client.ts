1fdb18a837dd2ff9702576fcd50a9882676fcd99
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-03-06 12:51:56 +0900

    [update] tidbとの接続用のprisma clientの生成

--- nextjs/prisma/client.ts
+++ nextjs/prisma/client.ts
@@ -1,10 +1,8 @@
 import { connect } from "@tidbcloud/serverless";
 import { PrismaTiDBCloud } from "@tidbcloud/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-// setup
-const connectionString = `${process.env.DATABASE_URL}`;
 
-// Initialize Prisma Client
+const connectionString = `${process.env.DATABASE_URL}`;
 const connection = connect({ url: connectionString });
 const adapter = new PrismaTiDBCloud(connection);
 const prisma = new PrismaClient({ adapter });
