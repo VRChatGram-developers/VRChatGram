023182bb9fcc6006b5ce50b8201cc5601ada491b
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-03-23 01:09:24 +0900

    [update] 画像投稿時には、既にs3へアップロードされた画像を登録するようにした

--- nextjs/app/api/v1/posts/route.ts
+++ nextjs/app/api/v1/posts/route.ts
@@ -1,23 +1,8 @@
 import { NextResponse } from "next/server";
 import { auth } from "@/libs/firebase/auth";
-import { S3Service } from "../../services/s3-service";
 import prisma from "@/prisma/client";
 
 export const runtime = "edge";
-
-const uploadImages = async (
-  images: { file_data: string; file_name: string; width: number; height: number }[]
-) => {
-  const s3Service = new S3Service();
-  return await Promise.all(
-    images.map(
-      async (image: { file_data: string; file_name: string; width: number; height: number }) => {
-        const url = await s3Service.uploadFileToS3(image.file_data, image.file_name);
-        return { url: url, width: image.width, height: image.height };
-      }
-    )
-  );
-};
 
 const formatBoothItems = (
   boothItems: { url: string; name: string; detail: string; image: string }[]
@@ -55,7 +40,6 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: "ユーザーが見つかりません" }, { status: 404 });
     }
 
-    const serializedImages = await uploadImages(images);
     const filteredTags = tags.filter((tag: string) => tag !== undefined && tag !== null);
 
     await prisma.posts.create({
@@ -66,7 +50,7 @@ export async function POST(request: Request) {
           create: formatBoothItems(boothItems),
         },
         images: {
-          create: serializedImages,
+          create: images,
         },
         tags: {
           create: filteredTags.map((tag: string) => ({
