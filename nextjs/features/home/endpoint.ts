e17e63f8d0bb3b6ff5c597401b74610d062fb3ac
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-03-28 22:06:41 +0900

    [update] buildエラー解消

--- nextjs/features/home/endpoint.ts
+++ nextjs/features/home/endpoint.ts
@@ -1,9 +1,9 @@
 import { Session } from "next-auth";
-import { Notifications } from "./types/notification";
+import { Notification } from "@/features/notifications/type/index";
 
 const API_GET_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const fetchNotifications = async (): Promise<Notifications | string> => {
+export const fetchNotifications = async (): Promise<Notification[] | string> => {
   const response = await fetch(`${API_GET_URL}/api/v1/notifications`);
   if (!response.ok) {
     console.error(response);
