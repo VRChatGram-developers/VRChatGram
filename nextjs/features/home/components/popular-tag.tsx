22a3b635ee3e871c78dba0394ef9689a2c1e0aae
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-04-07 01:46:30 +0900

    [update] typo修正

--- nextjs/features/home/components/popular-tag.tsx
+++ nextjs/features/home/components/popular-tag.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export const PopularTag = ({ popularTagList }: { popularTagList: Tag[] }) => {
   const router = useRouter();
 
-  const handleToPostSeacchList = () => {
+  const handleToPostSeachList = () => {
     router.push(`/posts?${new URLSearchParams({ page: "1" })}`);
   };
 
@@ -21,7 +21,7 @@ export const PopularTag = ({ popularTagList }: { popularTagList: Tag[] }) => {
           <div className={styles.avatarSearchButtonContent}>
             <div className={styles.avatarSearchTextContainer}>
               <p className={styles.avatarSearchTitle}>アバター写真</p>
-              <button className={styles.avatarSearchText} onClick={handleToPostSeacchList}>
+              <button className={styles.avatarSearchText} onClick={handleToPostSeachList}>
                 探す <span>›</span>
               </button>
             </div>
