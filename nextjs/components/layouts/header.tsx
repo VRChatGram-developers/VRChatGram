4bbb0c6c21139a55c48cac63fbf9b7a298b91603
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-04-13 21:48:24 +0900

    [add] 良いね一覧の導線を追加

--- nextjs/components/layouts/header.tsx
+++ nextjs/components/layouts/header.tsx
@@ -49,6 +49,10 @@ export const Header = () => {
     router.push(`/users/views`);
   };
 
+  const handleToMyFavoritePosts = () => {
+    router.push(`/users/likes`);
+  };
+
   const handleSearch = () => {
     const query = searchQuery.includes("#")
       ? createQueryParams({ tag: searchQuery, page: 1 })
@@ -186,6 +190,12 @@ export const Header = () => {
                     <div className={styles.moduleDrawerMenuSection}>
                       <p className={`${styles.moduleDrawerMenutext}`}>ダッシュボード</p>
                       <p className={`${styles.moduleDrawerMenutext}`}>自分の作品</p>
+                      <p
+                        className={`${styles.moduleDrawerMenutext}`}
+                        onClick={handleToMyFavoritePosts}
+                      >
+                        良いね一覧
+                      </p>
                       <p
                         className={`${styles.moduleDrawerMenutext}`}
                         onClick={handleToMyViewsPosts}
