45fac5bedf676ac80f983b78f284e88aaa76f154
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-05-14 23:55:22 +0900

    [update] refactor

--- nextjs/components/layouts/post-card.tsx
+++ nextjs/components/layouts/post-card.tsx
@@ -40,18 +40,6 @@ export const PostCard = ({ postCardProps }: { postCardProps: PostCardProps }) =>
     handleLikeOrUnlike,
   } = postCardProps;
 
-  const handleForwardToPostDetail = (postId: string | bigint) => {
-    const postIdString = typeof postId === "bigint" ? postId.toString() : postId;
-    router.prefetch(`/posts/${postIdString}`);
-    router.push(`/posts/${postIdString}`);
-  };
-
-  const handleForwardToUserDetail = (myId: string | bigint) => {
-    const myIdString = typeof myId === "bigint" ? myId.toString() : myId;
-    router.prefetch(`/users/${myIdString}`);
-    router.push(`/users/${myIdString}`);
-  };
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: _, status } = useSession();
   const handleClickLikeOrUnlike = () => {
