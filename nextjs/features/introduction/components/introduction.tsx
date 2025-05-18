2e6e77b92ab3157f320a66dd44dd1adfea642f10
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-05-19 03:08:04 +0900

    [add] 紹介ページを追加

---
+++ nextjs/features/introduction/components/introduction.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Introduction as IntroductionType } from "@/features/introduction/type/index";
+import styles from "../styles/introduction.module.scss";
+import parse from "html-react-parser";
+import Link from "next/link";
+
+export const Introduction = ({ introduction }: { introduction: IntroductionType }) => {
+  return (
+    <div className={styles.notificationDetailContainer}>
+      <div className={styles.notificationDetailContentContainer}>
+        <div className={styles.notificationDetailTitle}>{introduction.title}</div>
+        <div className={styles.notificationDetailContentText}>{parse(introduction.content)}</div>
+        <div className={styles.notificationDetailBackButton}>
+          <Link href="/">一覧へ戻る</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
