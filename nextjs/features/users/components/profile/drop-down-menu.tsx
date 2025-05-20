0a436cc1f53de20f3e428ff0553eb77a47b128bf
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-05-20 22:44:23 +0900

    [update] ブロック解除を実行できるようにした

--- nextjs/features/users/components/profile/drop-down-menu.tsx
+++ nextjs/features/users/components/profile/drop-down-menu.tsx
@@ -2,27 +2,45 @@
 
 import { useRef, useEffect } from "react";
 import styles from "@/features/users/styles/drop-down-menu.module.scss";
-import { blockUser } from "@/features/users/endpoint";
+import { blockUser, unblockUser } from "@/features/users/endpoint";
+
 type DropdownMenuProps = {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
   blockedUserId: string;
+  isBlocked: boolean;
+  setIsUserBlocked: (blocked: boolean) => void;
 };
 
-export const DropdownMenu = ({ isOpen, setIsOpen, blockedUserId }: DropdownMenuProps) => {
+export const DropdownMenu = ({
+  isOpen,
+  setIsOpen,
+  blockedUserId,
+  isBlocked,
+  setIsUserBlocked,
+}: DropdownMenuProps) => {
   const menuRef = useRef<HTMLDivElement>(null);
-  const googleFormLinks ="https://docs.google.com/forms/d/e/1FAIpQLSc2wPHJNSmD8tBIWMb6UDrJzlXNF3dYFx-okEQvITZvRXpOtQ/viewform";
+  const googleFormLinks =
+    "https://docs.google.com/forms/d/e/1FAIpQLSc2wPHJNSmD8tBIWMb6UDrJzlXNF3dYFx-okEQvITZvRXpOtQ/viewform";
 
   const handleReportUser = () => {
     window.open(googleFormLinks, "_blank");
   };
 
-  const handleBlockUser = async () => {
+  const handleBlockorUnblockUser = async () => {
+    const newBlockedState = !isBlocked;
+    setIsUserBlocked(newBlockedState); // UI をすぐに反映
+    setIsOpen(false); // メニューをすぐ閉じる
+
     try {
-      await blockUser(blockedUserId);
-      setIsOpen(false);
+      if (newBlockedState) {
+        await blockUser(blockedUserId);
+      } else {
+        await unblockUser(blockedUserId);
+      }
     } catch (error) {
-      console.error(error);
+      setIsUserBlocked(!newBlockedState);
+      console.error("API失敗", error);
     }
   };
 
@@ -47,12 +65,14 @@ export const DropdownMenu = ({ isOpen, setIsOpen, blockedUserId }: DropdownMenuP
   if (!isOpen) return null;
 
   return (
-    <div
-      ref={menuRef}
-      className={styles.menuContainer}
-    >
-      <div className={styles.menuItem} onClick={handleReportUser}>ユーザーを報告する</div>
-      <div className={styles.menuItem} onClick={handleBlockUser}>ユーザーをブロックする</div>
+    <div ref={menuRef} className={styles.menuContainer}>
+      <div className={styles.menuItem} onClick={handleReportUser}>
+        ユーザーを報告する
+      </div>
+
+      <div className={styles.menuItem} onClick={handleBlockorUnblockUser}>
+        {isBlocked ? "ブロックを解除する" : "ユーザーをブロックする"}
+      </div>
     </div>
   );
 };
