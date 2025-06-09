7da8dd495fc4058685b3d856a7ae9389c027f347
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-06-10 02:34:02 +0900

    [update] 未公開のお知らせは表示させない

--- nextjs/app/notifications/[id]/page.tsx
+++ nextjs/app/notifications/[id]/page.tsx
@@ -18,5 +18,10 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     ...notification,
     publishedAt: formattedDate,
   };
+
+  if (serializedNotification.publishedAt < new Date().toISOString()) {
+    return null;
+  }
+
   return <NotificationDetail notification={serializedNotification} />;
 }
