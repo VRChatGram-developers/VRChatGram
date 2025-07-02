812b015fec3b701175f4d387759cf4b3a41276bb
Kurehei4216 <kure.kohei.321@docomo.ne.jp>
2025-07-03 00:49:32 +0900

    [update] vrcssの利用方法のページのデザインを修正

--- nextjs/features/usage/components/usage-detail.tsx
+++ nextjs/features/usage/components/usage-detail.tsx
@@ -7,11 +7,11 @@ import Link from "next/link";
 
 export const UsageDetail = ({ usage }: { usage: Usage }) => {
   return (
-    <div className={styles.notificationDetailContainer}>
-      <div className={styles.notificationDetailContentContainer}>
-        <div className={styles.notificationDetailTitle}>{usage.title}</div>
-        <div className="prose">{parse(usage.content)}</div>
-        <div className={styles.notificationDetailBackButton}>
+    <div className={styles.usageDetailContainer}>
+      <div className={styles.usageDetailContentContainer}>
+        <div className={styles.usageDetailTitle}>{usage.title}</div>
+        <div className="prose max-w-full">{parse(usage.content)}</div>
+        <div className={styles.usageDetailBackButton}>
           <Link href="/">トップへ戻る</Link>
         </div>
       </div>
